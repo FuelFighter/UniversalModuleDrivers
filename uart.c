cb561ea1b38cc164b218a81b87bb062e223b051d
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-03-18 16:29:00 +0100

    Updated to receiving and transmitting driver, with adjustable baudrate and working on both uart0 and uart1.

--- uart.c
+++ uart.c
@@ -1,30 +1,73 @@
 /*
  * uart.c
  *
- * Created: 18/03/17 13:13:47
- *  Author: Sondre
- */ 
+ * Created: 14.03.2017 20.51.17
+ *  Author: mariu
+ */
 
 #include "uart.h"
 #include <avr/io.h>
-#include <stdint.h>
 
-void uart_init()
-{
-	const uint32_t baud_rate = 500000;
-	UBRR0 = (F_CPU / 16) / baud_rate - 1;
-	UCSR0C = (3 << UCSZ0);	// 8 bit transfer
-	UCSR0B |= (1 << TXEN0); // Enable transmit only
+// UART0
+void uart0_init (uint32_t baudrate) {
+	// Configure pin and port
+	DDRE |= (1 << PE1);
+	DDRE &= ~(1 << PE0);
+	
+	// Set Asynchronous Mode, 8bit size
+	UCSR0C &= ~(1 << UMSEL0);
+	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);
+	
+	UBRR0 = F_CPU / (16 * baudrate) - 1;
+	
+	// Enable transmit and receive
+	UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
 }
 
-void uart_tx_char(char c)
-{
-	while (!(UCSR0A & (1 << UDRE0))) { }; // Wait until data register is empty
-	UDR0 = c;
+uint16_t uart0_receive (void) {
+	// Check if there is data in receive buffer
+	if ((UCSR0A & (1 << RXC0))) {
+		// Return data from buffer
+		return UDR0;
+	}
+
+	return UART_NO_DATA;
 }
 
-void uart_tx_str(char* str)
-{
-	for(char* ptr = str; *ptr; ptr++)
-	uart_tx_char(*ptr);
+void uart0_transmit(uint8_t data) {
+	// Wait for messages to finish transmitting
+	while (!(UCSR0A & (1 << UDRE0))) { };
+	UDR0 = data;
+}
+
+// UART1
+void uart1_init (uint32_t baudrate) {
+	// Configure pin and port
+	DDRD |= (1 << PD3);
+	DDRD &= ~(1 << PD2);
+	
+	// Set Asynchronous Mode, 8bit size
+	UCSR1C &= ~(1 << UMSEL1);
+	UCSR1C |= (1 << UCSZ10) | (1 << UCSZ11);
+	
+	UBRR1 = F_CPU / (16 * baudrate) - 1;
+	
+	// Enable transmit and receive
+	UCSR1B |= (1 << RXEN1) | (1 << TXEN1);
+}
+
+uint16_t uart1_receive (void) {
+	// Check if there is data in receive buffer
+	if ((UCSR1A & (1 << RXC1))) {
+		// Return data from buffer
+		return UDR1;
+	}
+
+	return UART_NO_DATA;
+}
+
+void uart1_transmit(uint8_t data) {
+	// Wait for messages to finish transmitting
+	while (!(UCSR1A & (1 << UDRE1))) { };
+	UDR1 = data;
 }
--- uart.h
+++ uart.h
@@ -10,12 +10,31 @@
 #define UART_H_
 
 
+#include <stdint.h>
+
+
 #define F_CPU 8000000UL
 
+#define UART_NO_DATA (-1)
 
-void uart_init();
-void uart_tx_char(char c);
-void uart_tx_str(char* str);
+// Initialize UART0 with the given baudrate
+void uart0_init (uint32_t baudrate);
+
+// Transmit a single byte with UART0
+void uart0_transmit(uint8_t data);
+
+// Read the last received byte by UART0
+uint16_t uart0_receive (void);
+
+
+// Initialize UART1 with the given baudrate
+void uart1_init (uint32_t baudrate);
+
+// Transmit a single byte with UART1
+void uart1_transmit(uint8_t data);
+
+// Read the last received byte by UART0
+uint16_t uart1_receive (void);
 
 
 #endif /* UART_H_ */
