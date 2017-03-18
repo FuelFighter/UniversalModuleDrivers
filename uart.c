2a0ef1f558bd3b1620599591b52ec8ece77f7fd2
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-03-18 13:14:09 +0100

    Added uart.* empty files

---
+++ uart.c
@@ -0,0 +1,8 @@
+/*
+ * uart.c
+ *
+ * Created: 18/03/17 13:13:47
+ *  Author: Sondre
+ */ 
+
+#include "uart.h"
---
+++ uart.h
@@ -0,0 +1,16 @@
+/*
+ * uart.h
+ *
+ * Created: 18/03/17 13:13:39
+ *  Author: Sondre
+ */ 
+
+
+#ifndef UART_H_
+#define UART_H_
+
+
+
+
+
+#endif /* UART_H_ */
