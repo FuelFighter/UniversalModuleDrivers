fd6e0ec8e369efaa5357d4e9ea622a8acf3e6b3a
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-03-18 13:13:05 +0100

    Added can.* empty files

---
+++ can.c
@@ -0,0 +1,8 @@
+/*
+ * can.c
+ *
+ * Created: 18/03/17 13:12:06
+ *  Author: Sondre
+ */ 
+
+#include "can.h"
---
+++ can.h
@@ -0,0 +1,16 @@
+/*
+ * can.h
+ *
+ * Created: 18/03/17 13:12:17
+ *  Author: Sondre
+ */ 
+
+
+#ifndef CAN_H_
+#define CAN_H_
+
+
+
+
+
+#endif /* CAN_H_ */
