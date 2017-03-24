90e206d8083b91c8e82abcb79b6d17fdf03bcd0f
jorgeja <mrjackwitz@gmail.com>
2017-03-24 15:27:40 +0100

    Added usbdb drivers

---
+++ usbdb.c
@@ -0,0 +1,29 @@
+/*
+ * usbdb.c
+ *
+ * Created: 16.03.2017 20:55:52
+ *  Author: Ultrawack
+ */ 
+#include "usbdb.h"
+
+void usbdbg_init()
+{
+	const uint32_t baud_rate = 500000;
+	UBRR0 = (F_CPU / 16) / baud_rate - 1;
+	UCSR0C = (3 << UCSZ0);	// 8 bit transfer
+	UCSR0B |= (1 << TXEN0); // Enable transmit only
+	fdevopen(usbdbg_tx_char, NULL);
+}
+
+/*
+void usbdbg_tx_str(char* str)
+{
+	for(char* ptr = str; *ptr; ptr++)
+	usbdbg_tx_char(*ptr);
+}*/
+
+void usbdbg_tx_char(char c, FILE* stream)
+{
+	while (!(UCSR0A & (1 << UDRE0))) { }; // Wait until data register is empty
+	UDR0 = c;
+}
---
+++ usbdb.h
@@ -0,0 +1,23 @@
+/*
+ * usbdb.h
+ *
+ * Created: 16.03.2017 20:57:10
+ *  Author: Ultrawack
+ */ 
+
+
+#ifndef USBDB_H_
+#define USBDB_H_
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void usbdbg_init();
+
+/*void usbdbg_tx_str(char* str);*/
+
+void usbdbg_tx_char(char c, FILE* stream);
+
+#endif /* USBDB_H_ */
