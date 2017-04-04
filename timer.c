45425315c4c6b565a2c3a579da11a0bae1e053e2
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-04-04 19:50:28 +0200

    Add timer driver.
    The timer driver uses TC0 to generate regular interrupts, and uses these interrupts to maintain up to 8 timers.

---
+++ timer.c
@@ -0,0 +1,45 @@
+/*
+ * timer.c
+ *
+ * Created: 01/04/17 15:46:48
+ *  Author: Sondre
+ */ 
+
+#include "timer.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define NUMBER_OF_TIMERS 8
+
+static bool timer_enabled[NUMBER_OF_TIMERS];
+static uint32_t elapsed_microseconds[NUMBER_OF_TIMERS];
+
+void timer_init() {
+	// Configure timer with normal mode
+	TCCR0A = 0;
+	// Enable overflow interrupt
+	TIMSK0 = (1 << TOIE0);
+	// Enable timer with CLK_io/1024
+	TCCR0A |= 5;
+}
+
+void timer_start(timer_t timer) {
+	elapsed_microseconds[timer] = 0;
+	timer_enabled[timer] = true;
+}
+
+void timer_stop(timer_t timer) {
+	timer_enabled[timer] = false;
+}
+
+uint16_t timer_elapsed_ms(timer_t timer) {
+	return elapsed_microseconds[timer] / 1000;
+}
+
+ISR(TIMER0_OVF_vect) {
+	for (int t = 0; t < NUMBER_OF_TIMERS; t++) {
+		if (timer_enabled[t])	elapsed_microseconds[t] += (1000000ULL * 256 * 1024) / F_CPU;
+	}
+}
