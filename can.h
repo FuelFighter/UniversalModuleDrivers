15cf627a206c1da381dae274b2180fe01966b902
Jørgen Jackwitz <jorgejac@ntnu.no>
2017-04-23 16:18:36 +0200

    Added Definitions for CAN-ID's in can.h

--- can.h
+++ can.h
@@ -12,6 +12,22 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define BRAKE_CAN_ID			0x110
+#define ENCODER_CAN_ID			0x220
+#define STEERING_WHEEL_CAN_ID	0x230
+#define DASHBOARD_CAN_ID		0x310
+#define BMS_CELL_V_1_4_CAN_ID	0x440
+#define BMS_CELL_V_5_7_CAN_ID	0x441
+#define BMS_CELL_V_8_12_CAN_ID	0x442
+#define BMS_CELL_TEMP_CAN_ID	0x443
+#define BMS_VOLT_CURRENT_CAN_ID 0x444
+#define BMS_STATUS_CAN_ID		0x448
+#define BMS_ERROR_CAN_ID		0x449
+#define MOTOR_1_STATUS_CAN_ID	0x450
+#define MOTOR_2_STATUS_CAN_ID	0x460
+#define FRONT_LIGHTS_CAN_ID		0x470
+#define BACK_LIGHTS_CAN_ID		0x480
+
 typedef struct {
 	uint16_t id;
 	uint8_t length;
