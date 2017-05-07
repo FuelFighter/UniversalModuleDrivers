4d6b99a924f726eb7499a8adea197441ff6b98ce
jorgeja <mrjackwitz@gmail.com>
2017-05-07 14:04:57 +0200

    PWM.c/h har nå funksjoner for å endre prescale, sette duty cycle og endre top-verdien til timer 3. Timer 2 kan man ikke endre toppen til(alltid 0xFF)

--- pwm.c
+++ pwm.c
@@ -21,33 +21,145 @@
 
 void pwm_init(void){
 	
-	//OC3A & B pin as output
-	PORTE &= ~(1<<PE3);
-	DDRE |= (1<<PE3);
-	PORTE &= ~(1<<PE4);
-	DDRE |= (1<<PE4);
+	//Set pwm_pins as output;
+	PORTE &= ~((1<<PE3)|(1<<PE4)|(1<<PE5));
+	DDRE |= (1<<PE3)|(1<<PE4)|(1<<PE5);
+	
+	PORTB &= ~(1<<PB4);
+	DDRB |= (1<<PB4)
 	
 	//fast pwm, mode 14, TOP at ICR
-	set_bit(TCCR3B, WGM33);
-	set_bit(TCCR3B, WGM32);
-	set_bit(TCCR3A,WGM31);
-	clear_bit(TCCR3A,WGM30);
+	TCCR3B |= (1<<WGM33)|(1<<WGM32)|(1<<WGM31);
+	TCCR3B &= ~((1<<WGM31)|(1<<WGM30));
+	
+	//fast pwm, mode 3, Top at 0xFF
+	TCCR2A |= (1<<WGM21)|(1<<WGM20);
+	
 
 	// Set low on compare match for 3A&B
-	set_bit(TCCR3A, COM3A1);
-	clear_bit(TCCR3A, COM3A0);
-	set_bit(TCCR3A, COM3B1);
-	clear_bit(TCCR3A, COM3B0);
+	TCCR3A |= (1<<COM3A1)|(1<<COM3B1);
+	TCCR3A &= ~((1<<COM3A0)|(1<<COM3B0))
+	
+	TCCR2A |= (1<<COM2A1)
+	TCCR2A &=  ~((1<<COM2A0))
 	
 	//Set prescale clk/1 for timer 3
-	clear_bit(TCCR3B, CS32);
-	clear_bit(TCCR3B, CS31);
-	set_bit(TCCR3B, CS30);
+	
+	TCCR3B |= (1<<CS30);
+	TCCR3B &= ~((1<<CS32)|(1<<CS31));
 	
 	//Set top value for timer 3
 	ICR3 = 0xFF;
 	
-	//Set off 3A&B
+	//Set off 
 	OCR3A = 0x0000;
 	OCR3B = 0x0000;
+	OCR3C = 0x0000;
+	OCR2A = 0x0000;	
+}
+
+void pwm_set_duty_cycle(pwmPin_t pin, uint16_t dutyCycle)
+{
+	switch (pin)
+	{
+		case PWM_PB4:
+			OCR2A = (dutyCycle & 0xFF);
+			break;
+		case PWM_PE3:
+			OCR3A = (dutyCycle & 0xFFFF);
+			break;
+		case PWM_PE4:
+			OCR3B = (dutyCycle & 0xFFFF);
+			break;
+		case PWM_PE5:
+			OCR3C = (dutyCycle & 0xFFFF);
+		default:
+			break;
+	}
+}
+
+void pwm_set_top_t3(uint16_t top)
+{
+		ICR3 = (top & 0xFFFF);
+}
+
+void pwm_set_prescale(pwmPrescale_t scale, pwmTimer_t timer)
+{
+	switch (scale)
+	{
+		case DISABLE:
+			if (timer == PWM_T2)
+			{
+				TCCR2A &= ~((1<<CS22)|(1<<CS21)|(1<<CS0));
+				
+			} else if(timer == PWM_T3) {
+				TCCR3B &= ~((1<<CS32)|(1<<CS31)|(1<<CS30));
+			}
+			break;
+		case SCALE_1:
+			if (timer == PWM_T2)
+			{
+				TCCR2A &= ~((1<<CS22)|(1<<CS21));
+				TCCR2A |= ~((1<<CS20));
+			} else if(timer == PWM_T3) {
+				TCCR3B &= ~((1<<CS32)|(1<<CS31));
+				TCCR3B |= ~((1<<CS30));
+			} 
+			break;
+		case SCALE_8:
+			if (timer == PWM_T2)
+			{
+				TCCR2A &= ~((1<<CS22)|(1<<CS20));
+				TCCR2A |= ~((1<<CS21));
+			} else if(timer == PWM_T3) {
+				TCCR3B &= ~((1<<CS32)|(1<<CS30));
+				TCCR3B |= ~((1<<CS31));
+			}
+			break;
+		case SCALE_32_T2_ONLY:
+			if (timer == PWM_T2)
+			{
+				TCCR2A &= ~((1<<CS22));
+				TCCR2A |= ~((1<<CS21)|(1<<CS20));
+			} 
+			break;
+		case SCALE_64:
+			if (timer == PWM_T2)
+			{
+				TCCR2A &= ~((1<<CS21)|(1<<CS20));
+				TCCR2A |= ~((1<<CS22));
+			} else if(timer == PWM_T3) {
+				TCCR3B &= ~((1<<CS32));
+				TCCR3B |= ~((1<<CS30)|(1<<CS31));
+			}
+			break;
+		case SCALE_128_T2_ONLY:
+			if (timer == PWM_T2)
+			{
+				TCCR2A &= ~((1<<CS21));
+				TCCR2A |= ~((1<<CS22)|(1<<CS20));
+			} 
+			break;
+		case SCALE_256:
+			if (timer == PWM_T2)
+			{
+				TCCR2A &= ~((1<<CS20));
+				TCCR2A |= ~((1<<CS22)|(1<<CS21));
+			} else if(timer == PWM_T3) {
+				TCCR3B &= ~((1<<CS31)|(1<<CS30));
+				TCCR3B |= ~((1<<CS32));
+			}
+			break;
+		case SCALE_1024:
+			if (timer == PWM_T2)
+			{
+				TCCR2A |= ~((1<<CS22)|(1<<CS21)|(1<<CS20));
+			} else if(timer == PWM_T3) {
+				TCCR3B &= ~((1<<CS31));
+				TCCR3B |= ~((1<<CS32)|(1<<CS30));
+			}
+			break;
+		default:
+			break;
+	}
 }
--- pwm.h
+++ pwm.h
@@ -5,12 +5,39 @@
  *  Author: Ultrawack
  */ 
 
+#include <avr/io.h>
 
 #ifndef PWM_H_
 #define PWM_H_
 
 
+typedef enum {
+	PWM_PB4 = PB4,
+	PWM_PE3 = PE3,
+	PWM_PE4 = PE4,
+	PWM_PE5 = PE5,	
+}pwmPin_t;
+
+typedef enum {
+	PWM_T2;
+	PWM_T3;
+}pwmTimer_t;
+
+typedef enum {
+	DISABLE = 0,
+	SCALE_1 = 1,
+	SCALE_8,
+	SCALE_32_T2_ONLY,
+	SCALE_64,
+	SCALE_128_T2_ONLY,
+	SCALE_256,
+	SCALE_1024 
+}pwmPrescale_t;
+
 void pwm_init(void);
+void pwm_set_duty_cycle(pwmPin_t pin, uint16_t dutyCycle);
+void pwm_set_top(pwmPin_t pin, uint16_t top);
+void pwm_set_prescale(pwmPrescale_t scale, pwmTimer_t timer);
 
 
 #endif /* PWM_H_ */
